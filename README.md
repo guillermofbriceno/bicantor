4b468cdd47b6aefccb49801efa12d799df9557b8
guillermo <guillermofbriceno@gmail.com>
2022-02-11 23:55:14 -0700

    Add def file generator and README

---
+++ README.md
@@ -0,0 +1,1 @@
+## bicantor core
---
+++ util/defgen/defs.v
@@ -0,0 +1,17 @@
+`define UJ_MASK 32'h0000007f
+`define R_MASK 32'h00001234
+
+`define LUI_IDX 0
+`define AUIPC_IDX 1
+`define LODS_IDX 2
+
+`define WRB_REGF_MUX 1:0
+`define ALUOUT_SEL 0
+`define PC_P_4_SEL 1
+`define DATAMEM_SEL 2
+`define ALU_SRC1_MUX 2:2
+`define U_IMM_SEL 0
+`define RS1_SEL 1
+`define LUI_CTRL 7'b0101010
+`define AUIPC_CTRL 7'b1101100
+
